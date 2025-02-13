6daad47c363b349ace255fb333c30ace8cc0b77d
Kyle Widmann <kyle.widmann@gmail.com>
2025-02-12 19:53:14 -0500

    Updating broker to track subscriptions

--- pytrade/broker.py
+++ pytrade/broker.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Tuple
 
 from pytrade.data import CandleData
-from pytrade.instruments import Granularity, Instrument
+from pytrade.instruments import Candlestick, Granularity, Instrument
 from pytrade.interfaces.broker import IBroker
 from pytrade.interfaces.client import IClient
 from pytrade.interfaces.position import IPosition
@@ -14,7 +14,8 @@ class Broker(IBroker):
     def __init__(self, client: IClient):
         self.client = client
         self._orders: List[Order] = []
-        self._data_context = CandleData()
+        self._data_context = CandleData(max_size=100)
+        self._subscriptions: list[Tuple[Instrument, Granularity]] = []
 
     @property
     def equity(self) -> float:
@@ -47,8 +48,14 @@ class Broker(IBroker):
         self, instrument: Instrument, granularity: Granularity
     ) -> IInstrumentData:
 
+        key = (instrument, granularity)
+
         instrument_data = self._data_context.get(instrument, granularity)
         
-        self.client.subscribe(instrument, granularity, instrument_data.update)
+        # If we are already tracking this pair/granularity no need to resubscribe
+        if key not in self._subscriptions:
+            self.client.subscribe(instrument, granularity, instrument_data.update)
+            self._subscriptions.append(key)
 
         return instrument_data
+
